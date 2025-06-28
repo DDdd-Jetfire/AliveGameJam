b60a91d22023ca2249cbfc150161a2c1aed83928
卓泽 <wisteria_villosa_rehder@outlook.com>
2025-06-29 03:20:40 +0800

    视频上传

--- Assets/Scripts/UISystem/ButtonAnimation.cs
+++ Assets/Scripts/UISystem/ButtonAnimation.cs
@@ -1,16 +1,57 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
 public class ButtonAnimation : MonoBehaviour
     {
-
-
-    void Animation_rotate()
+    private Sequence currentSequence;
+    float currentX;
+    private void Start()
         {
-
+        currentX = this.transform.position.x;
         }
 
+    public void Animation_Rotate()
+        {
+        // 如果上一个旋转还在，先杀掉（可选）
+        //transform.DOKill();  // 只杀死该对象上的 tween，不影响位移动画
 
+        transform.DORotate(
+            new Vector3(0, 0, -360),  // 逆时针旋转
+            1f,
+            RotateMode.FastBeyond360
+        ).SetEase(Ease.Linear);
+        }
+
+    public void Animation_Back()
+        {
+        //KillCurrentSequence();
+        Debug.Log("后了");
+
+
+
+        currentSequence = DOTween.Sequence();
+        currentSequence.Append(transform.DOMoveX(currentX - 0.3f, 0.3f).SetEase(Ease.InQuad));
+        currentSequence.Append(transform.DOMoveX(currentX, 0.3f).SetEase(Ease.OutQuad));
+        }
+
+    public void Animation_Forward()
+        {
+        //KillCurrentSequence();
+        Debug.Log("移了");
+
+
+        currentSequence = DOTween.Sequence();
+        currentSequence.Append(transform.DOMoveX(currentX + 0.3f, 0.3f).SetEase(Ease.InQuad));
+        currentSequence.Append(transform.DOMoveX(currentX, 0.3f).SetEase(Ease.OutQuad));
+        }
+
+    private void KillCurrentSequence()
+        {
+        if (currentSequence != null && currentSequence.IsActive())
+            {
+            currentSequence.Kill();
+            currentSequence = null;
+            }
+        }
     }
