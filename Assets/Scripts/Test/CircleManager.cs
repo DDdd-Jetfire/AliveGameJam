10fc21cb2c6069377fdb9dbd96cf2b9f4b12611a
卓泽 <wisteria_villosa_rehder@outlook.com>
2025-06-29 14:43:26 +0800

    关卡3之后的UI, 关卡5, 猫咪资产上传

--- Assets/Scripts/Test/CircleManager.cs
+++ Assets/Scripts/Test/CircleManager.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 public class CircleManager : MonoBehaviour
     {
     public GameObject circlePrefab;
+    public GameObject Pivot;
     public Texture2D backgroundTexture;
 
     void Start()
         {
         Vector2 startPos = Vector2.zero;
         GameObject circle = Instantiate(circlePrefab, startPos, Quaternion.identity);
+        circle.transform.position = Pivot.transform.position;
+        circle.transform.localScale = Pivot.transform.localScale;
         circle.GetComponent<CircleBehavior>().Initialize(backgroundTexture);
         }
     }
